35591d8a2d1707249b0747637596894223f6e50d
Catharine <ekaterina.vasilko@mail.ru>
2021-12-27 21:27:22 +0500

    Главное меню (испр)

--- src/controller/EdController.kt
+++ src/controller/EdController.kt
@@ -10,13 +10,13 @@ class EdController {
     fun draw(g2: Graphics2D?, height : Int, width: Int) {
         g2?.color = Color.WHITE
         g2?.font = Font(Font.MONOSPACED, Font.BOLD, 30)
-        g2?.drawString("Стрелочки - передвижение в соответствующем напрвлении", width / 2 - off, height / 2)
-        g2?.drawString("ПКМ - начать перемещать объект", width / 2 - off, height / 2 + 30)
-        g2?.drawString("(необходимо получить умение)", width / 2 - off + 10, height / 2 + 60)
+        g2?.drawString("Стрелочки - передвижение в соответствующем напрвлении", 0, height / 2)
+        g2?.drawString("ПКМ - начать перемещать объект", 0, height / 2 + 30)
+        g2?.drawString("(необходимо получить умение)", 10, height / 2 + 60)
         g2?.drawString("ЛКМ - телепортация (необходимо получить умение)",
-                width / 2 - off, height / 2 + 90)
-        g2?.drawString("M - вызвать карту / свернуть карту", width / 2 - off, height / 2 + 120)
-        g2?.drawString("Esc - возврат в главное меню", width / 2 - off, height / 2 + 150)
+                0, height / 2 + 90)
+        g2?.drawString("M - вызвать карту / свернуть карту", 0, height / 2 + 120)
+        g2?.drawString("Esc - возврат в главное меню", 0, height / 2 + 150)
 
 
     }
